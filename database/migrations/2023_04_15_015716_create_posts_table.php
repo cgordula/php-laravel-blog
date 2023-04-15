6f3335437d69fd3050ea57d8174e4da44927139d
RevAdsCarmina <carmina@revenueads.com>
2023-04-14 21:28:24 -0500

    database migration

--- database/migrations/2023_04_15_015716_create_posts_table.php
+++ database/migrations/2023_04_15_015716_create_posts_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('content');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
         });
     }
