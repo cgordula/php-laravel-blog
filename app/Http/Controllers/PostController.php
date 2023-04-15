465dd5a8e36712345c490c8bc079837ae7f84152
RevAdsCarmina <carmina@revenueads.com>
2023-04-14 21:54:37 -0500

    creating route and controller for post creation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    // action to return a view containing a form for post creation
+    public function create() {
+        return view('posts.create');
+    }
 }
